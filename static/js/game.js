3ef1375af0de968dba0bbbf8fbb3f9700cd886f5
Spyros Tsattalios <spyros.tsattalios@gmail.com>
2022-06-07 21:20:51 +0300

    new commit

--- static/js/game.js
+++ static/js/game.js
@@ -2423,7 +2423,7 @@ class Game {
                // TAKE CARE OF DIFFERENT FRAME RATES (FPS). If a computer has i high frame rate i reduce the rain and movement speed whereas if it has a low one , i increase it
                // the speed and rain will change at every frame at the begining of the game but after some time (i.e. 350 iterations)
                // the frame rate will have become stable and hence we don't want to update them at every iteration to avoid instabilities
-               if (this.helpCounter < 350) {
+               if (this.helpCounter < 400) {
                     this.activatingRainNumber = Math.round(-83333.33 * this.deltaTime + 3083.33) // linear equation (y=ax+b) so that when delta=0.025 number = 1000 and when delta=0.007 number = 2500
                     this.movementSpeed = 18.8888 * this.deltaTime + 0.02777 // linear equation (y=ax+b) so that when delta=0.025 speed = 0.5 and when delta=0.007 speed = 0.16
                     this.rainVelocity = 28.8888 * this.deltaTime - 0.02222 // linear equation (y=ax+b) so that when delta=0.025 rain = 0.7 and when delta=0.007 rain = 0.18
